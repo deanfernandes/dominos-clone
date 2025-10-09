b862a3c595bb7220030b48e39774971ba843b9b9
dean <deanfernandes18@gmail.com>
2025-10-09 16:56:09 +0100

    change title; add AllergyAccordian component

--- src/components/menu/MenuBanner.tsx
+++ src/components/menu/MenuBanner.tsx
@@ -1,12 +1,12 @@
 import classes from "./MenuBanner.module.css";
-import menuBanner from "../../assets/menu_banner.jpg";
+import menuBanner from "../../assets/menu_banner.webp";
 
 export default function MenuBanner() {
   return (
     <div className={classes.menu_banner_container}>
       <a href="#pizzas" className={classes.banner_link}>
         <picture>
-          <source srcSet={menuBanner} type="image/jpeg" />
+          <source srcSet={menuBanner} type="image/webp" />
           <img src={menuBanner} alt="banner for pizza" />
         </picture>
       </a>
--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,10 +1,12 @@
 import classes from "./Menu.module.css";
 import MenuBanner from "../components/menu/MenuBanner";
+import AllergyInfoCard from "../components/menu/AllergyAccordian.tsx";
 
 export default function Menu() {
   return (
     <div>
       <MenuBanner />
+      <AllergyInfoCard />
     </div>
   );
 }
