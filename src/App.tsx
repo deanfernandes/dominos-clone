2a3e38d8147f056d5bf1d6a7d7b7a17e37ef3dfe
dean <deanfernandes18@gmail.com>
2025-10-24 13:09:59 +0100

    add navigate menu default page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
-import { Routes, Route } from "react-router";
+import { Routes, Route, Navigate } from "react-router";
 import Menu from "./pages/Menu";
 import Deals from "./pages/Deals";
 import ScrollToTop from "./components/ScrollToTop";
@@ -13,6 +13,7 @@ function App() {
       <Header />
       <main>
         <Routes>
+          <Route path="/" element={<Navigate to="/menu" />} />
           <Route path="/menu" element={<Menu />} />
           <Route path="/deals" element={<Deals />} />
         </Routes>
